84ceeee5bbe2135debc8905c16304f2968b795dd
deepkhatri050@gmail.com <deepkhatri050@gmail.com>
2025-05-24 20:18:41 +0530

    here we create some different type of  variables (int)

---
+++ c_lanuage/c-project/C-QuizGame.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int maiin(){
+  int i;
+  int ans1, ans2, ans3, ans4, ans5;
+  int point1, point2, point3, point4, point5, point6, point7, point8, point9, point10;
+  int point01, point02, point03, point04, point05, point06, point07, point08, point09, point010;
+  int total1, total2, total3;
+
+}
