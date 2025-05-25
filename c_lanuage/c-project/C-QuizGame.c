bdf7476f9fb1f70bbc437cdc83792994466898e2
deepkhatri050@gmail.com <deepkhatri050@gmail.com>
2025-05-25 11:08:13 +0530

    Just Solve Small Error And Make It Usefull

--- c_lanuage/c-project/C-QuizGame.c
+++ c_lanuage/c-project/C-QuizGame.c
@@ -1,152 +1,109 @@
 #include<stdio.h>
 
-int maiin(){
-  int i;
-  int ans1, ans2, ans3, ans4, ans5;
-  int point1, point2, point3, point4, point5, point6, point7, point8, point9, point10;
-  int point01, point02, point03, point04, point05, point06, point07, point08, point09, point010;
-  int total1, total2, total3;
+int main() {
+    int i;
+    int ans1, ans2, ans3, ans4, ans5;
+    int point1 = 0, point2 = 0, point3 = 0, point4 = 0, point5 = 0;
+    int total;
 
-  printf(" Wellcome to The Game\n\n");
-  printf("> Press 7 to Start The Game\n");
-  printf("Press 0 to Quit The Game\n");
+    printf("Welcome to The Game\n\n");
+    printf("> Press 7 to Start The Game\n");
+    printf("Press 0 to Quit The Game\n");
 
-  scanf("%d", &i);
+    scanf("%d", &i);
 
-  if(i==7)
-  {
-    printf("The Game Has Started \n\n");
-  }
-  else if(i==0)
-  {
-    printf("The Game Has Ended\n\n");
-  }
-  else
-  {
-     printf("Invalid\n\n");
-  }
-  
-  if(i==7)
-  {
+    if(i == 7) {
+        printf("The Game Has Started \n\n");
+    } else if(i == 0) {
+        printf("The Game Has Ended\n\n");
+        return 0;
+    } else {
+        printf("Invalid\n\n");
+        return 0;
+    }
+
     printf("1) Which one Is The First Search Engine In Internet?\n");
     printf("a) Google\n");
     printf("b) Archie\n");
     printf("c) wais\n");
     printf("d) Altavista\n");
-
-    printf("Enter Your Answer : ");
+    printf("Enter Your Answer (1-4): ");
     scanf("%d", &ans1);
+    if(ans1 == 2) {
+        printf("Correct Answer\n");
+        point1 = 5;
+    } else {
+        printf("Wrong Answer\n");
+    }
+    printf("You Have Scored %d Point\n\n", point1);
 
-    if(ans1==2)
-    {
-      printf("Correct Answer\n");
-      point1 = 5;
-      printf("You Have Score %d Point\n\n", point1); 
-    }
-    else
-      {
-      printf("Worng Answer\n");
-      point01 = 0;
-      printf("You Have Score %d Point\n\n", point01); 
-    }
     printf("2) Which one Is The First Computer?\n");
     printf("a) ENIAC\n");
     printf("b) UNIVAC\n");
     printf("c) IBM\n");
     printf("d) Apple\n");
-
-    printf("Enter Your Answer : ");
+    printf("Enter Your Answer (1-4): ");
     scanf("%d", &ans2);
+    if(ans2 == 1) {
+        printf("Correct Answer\n");
+        point2 = 5;
+    } else {
+        printf("Wrong Answer\n");
+    }
+    printf("You Have Scored %d Point\n\n", point2);
 
-    if(ans2==1)
-    {
-      printf("Correct Answer\n");
-      point2 = 5;
-      printf("You Have Score %d Point\n\n", point2); 
-    }
-    else
-    {
-      printf("Worng Answer\n");
-      point02 = 0;
-      printf("You Have Score %d Point\n\n", point02); 
-    }
     printf("3) Which one Is The First Computer Virus?\n");
     printf("a) Brain\n");
     printf("b) Creeper\n");
     printf("c) Elk Cloner\n");
     printf("d) Sasser\n");
-    
-    printf("Enter Your Answer : ");
+    printf("Enter Your Answer (1-4): ");
     scanf("%d", &ans3);
-    if(ans3==1)
-    {
-      printf("Correct Answer\n");
-      point3 = 5;
-      printf("You Have Score %d Point\n\n", point3); 
+    if(ans3 == 1) {
+        printf("Correct Answer\n");
+        point3 = 5;
+    } else {
+        printf("Wrong Answer\n");
     }
-    else
-    {
-      printf("Worng Answer\n");
-      point03 = 0;
-      printf("You Have Score %d Point\n\n", point03); 
-    }
+    printf("You Have Scored %d Point\n\n", point3);
 
     printf("4) Which one Is The First Computer Game?\n");
     printf("a) Tetris\n");
     printf("b) Pong\n");
     printf("c) Spacewar\n");
     printf("d) Pacman\n");
-
-    printf("Enter Your Answer : ");
+    printf("Enter Your Answer (1-4): ");
     scanf("%d", &ans4);
-    if(ans4==3)
-    {
-      printf("Correct Answer\n");
-      point4 = 5;
-      printf("You Have Score %d Point\n\n", point4); 
+    if(ans4 == 3) {
+        printf("Correct Answer\n");
+        point4 = 5;
+    } else {
+        printf("Wrong Answer\n");
     }
-    else
-    {
-      printf("Worng Answer\n");
-      point04 = 0;
-      printf("You Have Score %d Point\n\n", point04); 
-    }
+    printf("You Have Scored %d Point\n\n", point4);
+
     printf("5) Which one Is The First Computer Language?\n");
     printf("a) Fortran\n");
     printf("b) C\n");
     printf("c) Assembly\n");
     printf("d) Cobol\n");
-
-    printf("Enter Your Answer : ");
+    printf("Enter Your Answer (1-4): ");
     scanf("%d", &ans5);
-    if(ans5==3)
-    {
-      printf("Correct Answer\n");
-      point5 = 5;
-      printf("You Have Score %d Point\n\n", point5); 
+    if(ans5 == 3) {
+        printf("Correct Answer\n");
+        point5 = 5;
+    } else {
+        printf("Wrong Answer\n");
     }
-    else
-    {
-      printf("Worng Answer\n");
-      point05 = 0;
-      printf("You Have Score %d Point\n\n", point05); 
+    printf("You Have Scored %d Point\n\n", point5);
+
+    total = point1 + point2 + point3 + point4 + point5;
+    printf("You Have Scored %d Points\n", total);
+    if(total >= 20) {
+        printf("You Are The Winner\n");
+    } else {
+        printf("You Are The Loser\n");
     }
 
-    total1 = point1 + point2 + point3 + point4 + point5;
-    total2 = point01 + point02 + point03 + point04 + point05;
-    total3 = total1 - total2;
-    printf("You Have Score %d Point\n", total3);
-    if(total3 >= 20)
-    {
-      printf("You Are The Winner\n");
-    }
-    else
-    {
-      printf("You Are The Looser\n");
-    }
-
-
-  }
-  return 0;
+    return 0;
 }
-
